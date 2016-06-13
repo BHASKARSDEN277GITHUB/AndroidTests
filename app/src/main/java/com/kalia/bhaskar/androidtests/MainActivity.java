4aa34d43adce96be529013de3d072c52563057f6
Bhaskar Kalia <bhaskar.kalia@snapdeal.com>
2016-06-13 23:37:14 +0530

    Initial Commit (Speech to text recognition)

---
+++ app/src/main/java/com/kalia/bhaskar/androidtests/MainActivity.java
@@ -0,0 +1,47 @@
+package com.kalia.bhaskar.androidtests;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    protected static final int REQUEST_OK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+        try {
+            //this will return result from the called activity which
+            //can be captured at onActivityResult method
+            startActivityForResult(i, REQUEST_OK);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error initializing speech to text engine.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_OK && resultCode == RESULT_OK) {
+            ArrayList<String> speech = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            ((TextView) findViewById(R.id.text1)).setText(speech.get(0));
+        }
+    }
+}
